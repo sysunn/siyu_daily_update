bac637d7d5249755f4a01d058168db753f701e4b
Siyu Sun <71289781+sysunn@users.noreply.github.com>
2023-12-20 12:53:08 -0500

    Update MEME_individual_from_db_python3.py

--- December_2023/MEME_individual_from_db_python3.py
+++ December_2023/MEME_individual_from_db_python3.py
@@ -11,6 +11,8 @@ def separate_motifs(filename):
         strands = ''
         motif_letter_freq = ''
         background_letter_freq = ''
+        weight_matrix = ''
+        in_weight_matrix = False
         
         while True:
             line = infile.readline()
@@ -24,46 +26,47 @@ def separate_motifs(filename):
                 alphabet = line
             elif line.startswith('strands:'):
                 strands = line
-            elif line.startswith('Letter frequencies in dataset:'):
-                motif_letter_freq = line
-                next_line = infile.readline().strip()
-                while next_line:
-                    motif_letter_freq += '\n' + next_line
-                    next_line = infile.readline().strip()
             elif line.startswith('Background letter frequencies'):
                 background_letter_freq = line
                 next_line = infile.readline().strip()
                 while next_line:
                     background_letter_freq += '\n' + next_line
                     next_line = infile.readline().strip()
+            elif line.startswith('letter-probability matrix'):
+                weight_matrix = line
+                in_weight_matrix = True
+                next_line = infile.readline().strip()
+                while next_line:
+                    weight_matrix += '\n' + next_line
+                    next_line = infile.readline().strip()
+            elif line.startswith('Stopped because maximum number of motifs'):
+                in_weight_matrix = False
             
             if line.startswith('MOTIF'):
                 if current_motif_name:
-                    write_motif_to_file(current_motif_name, current_motif_lines, meme_version, alphabet, strands, motif_letter_freq, background_letter_freq)
+                    write_motif_to_file(current_motif_name, current_motif_lines, meme_version, alphabet, strands, background_letter_freq, weight_matrix)
                 current_motif_name = line.split()[1]
                 current_motif_lines = [line]
-            else:
+                in_weight_matrix = False
+            elif not in_weight_matrix:
                 current_motif_lines.append(line)
         
         # Write the last motif
         if current_motif_name:
-            write_motif_to_file(current_motif_name, current_motif_lines, meme_version, alphabet, strands, motif_letter_freq, background_letter_freq)
+            write_motif_to_file(current_motif_name, current_motif_lines, meme_version, alphabet, strands, background_letter_freq, weight_matrix)
 
-def write_motif_to_file(motif_name, motif_lines, meme_version, alphabet, strands, motif_letter_freq, background_letter_freq):
-    with open(motif_name + '_meme.txt', 'w') as outfile:
-        outfile.write(meme_version + '\n')
+def write_motif_to_file(motif_name, motif_lines, meme_version, alphabet, strands, background_letter_freq, weight_matrix):
+    with open(motif_name + '_output.txt', 'w') as outfile:
+        outfile.write(meme_version + '\n\n')
         outfile.write(alphabet + '\n\n')
         outfile.write(strands + '\n\n')
-        outfile.write(motif_letter_freq + '\n')
         outfile.write(background_letter_freq + '\n\n')
 
-        # Extract motif-specific information
-        motif_content = '\n'.join(motif_lines)
-        motif_info = re.search(r'MOTIF\s+{}(.*?)Time'.format(motif_name), motif_content, re.DOTALL)
-        if motif_info:
-            motif_content = motif_info.group(1)
-            outfile.write('MOTIF\t' + motif_name + '\n')
-            outfile.write(motif_content.strip())
+        for line in motif_lines:
+            outfile.write(line + '\n')
+
+        outfile.write('\n\n')
+        outfile.write(weight_matrix + '\n')
 
 def main():
     if len(sys.argv) != 2:
